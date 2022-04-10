039566e044ad05fadf026a73c065b91958f18e34
ArcticBlueOrange <brando1411@gmail.com>
2022-04-10 15:29:57 +0200

    cart

---
+++ src/app/features/cart/cart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ac-cart',
+  templateUrl: './cart.component.html',
+  styles: [
+  ]
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submitHandler(value:any) {
+
+  }
+
+}
